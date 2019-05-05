156c19574947b2d8ae8fda511dffc3bf06de7c30
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2019-05-05 15:41:11 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    // std::cout << "Requires argument(s): " << std::endl;
+    // std::cout << "\tint n: problemsize" << std::endl;
+    // throw std::logic_error("Wrong program arguments! ");
+  }
+  return 0;
+}
